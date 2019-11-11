00db7ccebe64123bc49794d92b4c8ca81f83de5a
neohuang <superneohuang@gmail.com>
2019-11-11 16:39:08 +0100

    Add panic handler

--- adjust_server/server.go
+++ adjust_server/server.go
@@ -9,18 +9,25 @@ import (
 )
 
 type AdjustServer struct {
-	Version string
+	Version   string
+	panicChan chan struct{}
 }
 
 func New(version string) *AdjustServer {
 	server := &AdjustServer{
-		Version: version,
+		Version:   version,
+		panicChan: make(chan struct{}),
 	}
 	return server
 }
 
 func (server *AdjustServer) Start() {
 	mux := server.ServeMux()
+	go func() {
+		<-server.panicChan
+		log.Panicf("don't panic.....")
+	}()
+
 	if err := http.ListenAndServe(":80", mux); err != nil {
 		log.Panicf("ADJUST SERVER SHUTTING DOWN (%s)\n\n", err)
 	}
@@ -31,6 +38,9 @@ func (server *AdjustServer) ServeMux() *http.ServeMux {
 	mux.Handle("/metrics", promhttp.Handler())
 	mux.Handle("/heartbeat", handlers.NewHeartbeatHandler())
 	mux.Handle("/version", handlers.NewVersionHandler(server.Version))
+	mux.HandleFunc("/panic", func(http.ResponseWriter, *http.Request) {
+		close(server.panicChan)
+	})
 
 	return mux
 }
