7ffabe1e61a6f3c244b3d7aacd5ad10595a00687
neohuang <superneohuang@gmail.com>
2019-11-20 11:22:09 +0100

    Update version handler

--- handlers/version_handler.go
+++ handlers/version_handler.go
@@ -3,19 +3,23 @@ package handlers
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 type VersionHandler struct {
 	version string
+	podName string
 }
 
 func NewVersionHandler(version string) *VersionHandler {
+	podName := os.Getenv("POD_NAME")
 	handler := &VersionHandler{
 		version: version,
+		podName: podName,
 	}
 	return handler
 }
 
 func (handler *VersionHandler) ServeHTTP(writer http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(writer, `{"version": "%s"}`, handler.version)
+	fmt.Fprintf(writer, `{"version": "%s", "pod_name":"%s"}`, handler.version, handler.podName)
 }
